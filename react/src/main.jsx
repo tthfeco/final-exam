ce1f09fa2930b275d534c80e1da3d30087a4066f
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2024-06-25 11:54:03 +0200

    create 2 path in main.jsx

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { initFakeApi } from "./mirage.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import RoomDetails from "./Pages/RoomDetails.jsx";
+
 import "./index.css";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/details/:id",
+    element: <RoomDetails />,
+  },
+]);
 
 // don't touch this line
 initFakeApi();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </React.StrictMode>
 );
